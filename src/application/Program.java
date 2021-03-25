3f6ee6e8d72e34c34d92de6b97203b1ccc18bd15
Wagner <wagneer.alm@gmail.com>
2021-03-25 00:30:14 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,8 @@
 package application;
 
 import java.util.Date;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -20,6 +22,8 @@ public class Program {
         Department obj = new Department(1, "Books");
         
         Seller seller = new Seller(21, "Wagner", "wagner@email.com", new Date(),3000.2, obj);
+        
+        SellerDao sellerDao = DaoFactory.creaSellerDao();
         
         System.err.println(seller);
 
